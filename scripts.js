47e64bee6e35be4cf647f1168669894a9fafc656
Brian Beal <brianwbeal@gmail.com>
2018-11-10 20:11:40 -0800

    create basic logic for script. take in values of inputs in html form, then run an if statement to determine which operator was selected, then complete the requested math, and finally report the answer.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,26 @@
 $(document).ready(() => {
 
-  alert('working!')
+  $("#calculator").submit(() => {
 
+    $("#showResult").empty();
+
+    let firstValue = parseInt($("input#first").val());
+    let operatorValue = $("#operator").val();
+    let secondValue = parseInt($("input#second").val());
+    let resultValue = 0;
+
+    if (operatorValue === "add") {
+      resultValue = (firstValue + secondValue);
+    } else if (operatorValue === "subtract") {
+      resultValue = (firstValue - secondValue);
+    } else if (operatorValue === "multiply") {
+      resultValue = (firstValue * secondValue);
+    } else if (operatorValue === "divide") {
+      resultValue = (firstValue / secondValue);
+    }
+
+    $("#showResult").append(resultValue);
+
+    event.preventDefault();
+  });
 });
