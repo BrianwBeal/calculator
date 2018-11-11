5a3964b0bfc19f75da766f04f61c351557d4ddd7
Brian Beal <brianwbeal@gmail.com>
2018-11-10 20:31:15 -0800

    create logic for calculator to add, subtract, multiply, and divide.

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ $(document).ready(() => {
 
   $("#calculator").submit(() => {
 
+    $("#showRequest>span").empty();
     $("#showResult").empty();
 
     let firstValue = parseInt($("input#first").val());
@@ -11,12 +12,32 @@ $(document).ready(() => {
 
     if (operatorValue === "add") {
       resultValue = (firstValue + secondValue);
+      $("#showOperatorValue").append(operatorValue);
+      $("#showFirstValue").append(firstValue);
+      $("#etc").append("and");
+      $("#showSecondValue").append(secondValue);
+
     } else if (operatorValue === "subtract") {
       resultValue = (firstValue - secondValue);
+      $("#showOperatorValue").append(operatorValue);
+      $("#showFirstValue").append(secondValue);
+      $("#etc").append("from");
+      $("#showSecondValue").append(firstValue);
+
     } else if (operatorValue === "multiply") {
       resultValue = (firstValue * secondValue);
+      $("#showOperatorValue").append(operatorValue);
+      $("#showFirstValue").append(firstValue);
+      $("#etc").append("by");
+      $("#showSecondValue").append(secondValue);
+
     } else if (operatorValue === "divide") {
       resultValue = (firstValue / secondValue);
+      $("#showOperatorValue").append(operatorValue);
+      $("#showFirstValue").append(firstValue);
+      $("#etc").append("by");
+      $("#showSecondValue").append(secondValue);
+
     }
 
     $("#showResult").append(resultValue);
